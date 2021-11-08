0173772b606265e9f0b031e7a578b34688f10ae0
Diego Andrés <diegoandres_cortes@outlook.com>
2021-11-07 19:48:46 -0600

    gh release
    Signed-off-by: Diego Andrés <diegoandres_cortes@outlook.com>

---
+++ languages/ThemeStrings.english.php
@@ -0,0 +1,20 @@
+<?php
+// Version: 2.0; ThemeStrings
+
+$txt['rs_comments'] = 'This topic has no comments, be the first.';
+$txt['rs_account'] = 'My account';
+$txt['rs_profile'] = 'Edit profile';
+$txt['rs_post'] = 'New posts';
+$txt['rs_replies'] = 'New replies';
+$txt['icons_check'] = 'Enable social icons';
+$txt['rs_facebook'] = 'Facebook';
+$txt['facebook_check'] = 'Enable icon of Facebook';
+$txt['facebook_text'] = 'URL of the page to Facebook';
+$txt['rs_twitter'] = 'Twitter';
+$txt['twitter_check'] = 'Enable icon of Twitter';
+$txt['twitter_text'] = 'URL of the page to Twitter';
+$txt['rs_youtube'] = 'Youtube';
+$txt['youtube_check'] = 'Enable icon of Youtube';
+$txt['youtube_text'] = 'URL of the page to Youtube';
+
+?>
